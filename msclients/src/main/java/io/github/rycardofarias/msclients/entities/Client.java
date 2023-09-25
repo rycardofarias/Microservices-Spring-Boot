e8e5d42bfa7398527a4298cb0f48f79afdb292b6
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-25 00:57:04 -0300

    Client gets e sets

--- msclients/src/main/java/io/github/rycardofarias/msclients/entities/Client.java
+++ msclients/src/main/java/io/github/rycardofarias/msclients/entities/Client.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Client {
 
@@ -23,5 +25,50 @@ public class Client {
         this.cpf = cpf;
         this.nome = nome;
         this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
