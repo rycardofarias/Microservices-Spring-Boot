75e4436430070c852dd26fe4f28f115c043f2792
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-30 16:11:26 -0300

    Use spring boot version 2.7

--- msclients/src/main/java/io/github/rycardofarias/msclients/MsclientsApplication.java
+++ msclients/src/main/java/io/github/rycardofarias/msclients/MsclientsApplication.java
@@ -2,10 +2,10 @@ package io.github.rycardofarias.msclients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class MsclientsApplication {
 
 	public static void main(String[] args) {
--- msclients/src/main/java/io/github/rycardofarias/msclients/entities/Client.java
+++ msclients/src/main/java/io/github/rycardofarias/msclients/entities/Client.java
@@ -1,10 +1,10 @@
 package io.github.rycardofarias.msclients.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
